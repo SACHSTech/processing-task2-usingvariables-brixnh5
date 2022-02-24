ae1fdacb7b1d2eaa5b0b3444d4c94fe4131d4c8b
brixnh5 <brian.huang23@ycdsbk12.ca>
2022-02-23 22:25:36 -0500

    first commit

--- Sketch.java
+++ Sketch.java
@@ -2,35 +2,78 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
+    int intWidth = 800;
+    int intHeight = 800;
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
+    public void settings() {
+      size(intWidth, intHeight);
+    }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    public void setup() {
+      background(210, 255, 173);
+    }
   
-  // define other methods down here.
-}
+    public void draw() {
+      //sky
+      noStroke();
+      fill(137, 207, 240);
+      rect(intWidth * 0, intHeight * 0, intWidth, intHeight/2);
+      
+      //sun
+      fill(250, 253, 15);
+      ellipse(intWidth * 0, intHeight * 0, intWidth/4, intHeight/4);
+  
+      //house
+      fill(237, 201, 175);
+      rect((float)(intWidth * 0.275), (float)(intHeight * 0.3125), (float)(intWidth * 0.4375), (float)(intHeight * 0.4375));
+  
+      //roof
+      fill(72, 72, 72);
+      triangle(intWidth/5, (float)(intHeight * 0.375), (float)(intWidth * 0.7875), (float)(intHeight * 0.375), (float)(intWidth * 0.49375), intHeight/8);
+  
+      //door
+      fill(66, 40, 14);
+      rect((float)(intWidth * 0.55), (float)(intHeight * 0.575), intWidth/10, (float)(intHeight * 0.175));
+  
+      //doorknob
+      stroke(0, 0, 0);
+      fill(250, 253, 15);
+      ellipse((float)(intWidth * 0.6325), (float)(intHeight * 0.6625), intWidth/80, intHeight/80);
+  
+      //cloud 1
+      noStroke();
+      fill(251, 255, 255);
+      ellipse((float)(intWidth * 0.75), intHeight/8, (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      fill(251, 255, 255);
+      ellipse((float)(intWidth * 0.8), intHeight/8, (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      fill(251, 255, 255);
+      ellipse((float)(intWidth * 0.85), intHeight/8, (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      fill(251, 255, 255);
+      ellipse((float)(intWidth * 0.7775), (float)(intHeight * 0.0875), (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      fill(251, 255, 255);
+      ellipse((float)(intWidth * 0.8225), (float)(intHeight * 0.0875), (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      //cloud 2
+      fill(251, 255, 255);
+      ellipse((float)(intWidth * 0.15), intHeight/4, (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      fill(251, 255, 255);
+      ellipse(intWidth/5, intHeight/4, (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      fill(251, 255, 255);
+      ellipse(intWidth/4, intHeight/4, (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      fill(251, 255, 255);
+      ellipse((float)(intWidth * 0.1775), (float)(intHeight * 0.2125), (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+  
+      fill(251, 255, 255);
+      ellipse((float)(intWidth * 0.2225), (float)(intHeight * 0.2125), (float)(intWidth * 0.075), (float)(intHeight * 0.075));
+      
+    }
+    
+    // define other methods down here.
+  }
